d97a92f83f3dfc191e7a9096dde6d6c91ea26014
presw <pmwhitehall@gmail.com>
2019-07-29 15:36:57 -0700

    WIP - ES6 style

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // TODO
+var groceryItems = [{food: 'cough drops'}, {food: 'cucumbers'}, {food: 'chocolate'}];
+
+var GroceryListItem = (props) =>
+  return <li >{props.food}</li>
+);
+
+var GroceryList = (props) => {
+  var items = props.food;
+  var listItems = items.map((item) =>
+    <GroceryListItem key={food} value={food.item} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+};
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList />
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
